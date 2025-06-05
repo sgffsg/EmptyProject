d2bdcdce077c8be0f7b1f8cf55304a163ee4f144
sgffsg <pichugov-sasha@mail.ru>
2025-06-05 22:28:22 +0300

    Add main files

---
+++ include/Demo.h
@@ -0,0 +1,16 @@
+//
+// Created by Sakata on 05.06.2025.
+//
+
+#ifndef DEMO_H
+#define DEMO_H
+
+
+
+class Demo {
+
+};
+
+
+
+#endif //DEMO_H
---
+++ src/Demo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sakata on 05.06.2025.
+//
+
+#include "../include/Demo.h"
